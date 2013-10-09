ac9090d5721feaa8e3af10b3c48bd1b93739fc07
xrowrobert <robert@xrow.de>
2013-10-09 16:40:58 +0200

    - sql fixed

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,7 +1,1 @@
-CREATE TABLE dwd_archive (
-  "id" INT NOT NULL AUTO_INCREMENT,
-  "day_clean" VARCHAR(45) NOT NULL ,
-  "write_ts" VARCHAR(45) NOT NULL ,
-  "temperature" VARCHAR(45) NOT NULL ,
-  "state" VARCHAR(45) NOT NULL ,
-  PRIMARY KEY ("id") );
+CREATE TABLE dwd_archive ( id INT NOT NULL AUTO_INCREMENT, day_clean VARCHAR(45) NOT NULL , write_ts VARCHAR(45) NOT NULL , temperature VARCHAR(45) NOT NULL ,  state VARCHAR(45) NOT NULL ,   PRIMARY KEY (id) );
